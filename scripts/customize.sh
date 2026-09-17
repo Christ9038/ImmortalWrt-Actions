ebaa16196fb98c0e871d7ba62d9655d51b92e7ee
Christ <Christ9038@gmail.com>
2026-09-17 13:15:37 +0800

    fix: configure working PassWall2 feeds

--- scripts/customize.sh
+++ scripts/customize.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-PASSWALL2_FEED="https://github.com/xiaorouji/openwrt-passwall2.git"
+PASSWALL2_FEED="https://github.com/Openwrt-Passwall/openwrt-passwall2.git"
+PASSWALL_PACKAGES_FEED="https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git"
 MOSDNS_FEED="https://github.com/sbwml/luci-app-mosdns.git"
 
 grep -qF "src-git passwall2 ${PASSWALL2_FEED}" feeds.conf.default \
   || echo "src-git passwall2 ${PASSWALL2_FEED}" >> feeds.conf.default
+grep -qF "src-git passwall_packages ${PASSWALL_PACKAGES_FEED}" feeds.conf.default \
+  || echo "src-git passwall_packages ${PASSWALL_PACKAGES_FEED}" >> feeds.conf.default
 grep -qF "src-git mosdns ${MOSDNS_FEED};v5" feeds.conf.default \
   || echo "src-git mosdns ${MOSDNS_FEED};v5" >> feeds.conf.default
 
